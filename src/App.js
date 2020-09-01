bafeedd5018440a1ab8f3af2fffce45944e7000d
Colin Courtney <ccour004@gmail.com>
2020-09-01 13:57:02 -0400

    Minor update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,57 @@
 import React from 'react';
-import { VictoryChart,VictoryArea,VictoryTheme } from "victory";
+import ChartistGraph from 'react-chartist';
 import { Button } from '@material-ui/core';
 import ApiCalendar from 'react-google-calendar-api';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-    <Button color="primary" onClick={()=>ApiCalendar.handleAuthClick()}>Sign in</Button>
-    <Button color="primary" onClick={()=>ApiCalendar.handleSignoutClick()}>Sign out</Button>
-    <VictoryChart
-    theme={VictoryTheme.material}
-    >
-      <VictoryArea
-        style={{ data: { fill: "#c43a31" } }}
-        data={[
-          { x: 1, y: -2},
-          { x: 2, y: 3},
-          { x: 3, y: 5},
-          { x: 4, y: 4},
-          { x: 5, y: 6}
-        ]}
-      />
-    </VictoryChart>
-    </div>
-  );
+class Bar extends React.Component {
+  render() {
+ 
+    var data = {
+      labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+      series: [
+        [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+      ]
+    };
+ 
+    var options = {
+      high: 10,
+      low: -10,
+      axisX: {
+        labelInterpolationFnc: function(value, index) {
+          return index % 2 === 0 ? value : null;
+        }
+      }
+    };
+ 
+    var type = 'Line'
+ 
+    return (
+      <div>
+        <ChartistGraph data={data} options={options} type={type} />
+      </div>
+    )
+  }
+}
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {isSigned:false};
+    ApiCalendar.onLoad(() => {
+      console.log("Calendar API loaded...")
+      ApiCalendar.listenSign((isSigned)=>{console.log("LISTEN SIGNED! "+isSigned);this.setState({isSigned})});
+    });
+  }
+
+  render(){
+    return this.state.isSigned ? 
+      <div>
+      <Button color="primary" onClick={()=>ApiCalendar.handleSignoutClick()}>Sign out</Button>
+      <Bar/> 
+      </div>
+    : <Button color="primary" onClick={()=>ApiCalendar.handleAuthClick()}>Sign in</Button>
+  }
 }
 
 export default App;
